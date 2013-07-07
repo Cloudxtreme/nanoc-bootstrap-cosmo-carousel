a9adf1be31b922efa955c4ba3c27c516a9d792c5
n0stromo <barfek@gmail.com>
2013-07-07 15:15:24 +0200

    Uploading template base files.

---
+++ content/assets/js/application.js
@@ -0,0 +1,35 @@
+//---------------------------------------------
+//  JS COMPONENTS LOADER
+//---------------------------------------------
+//---------------------------------------------
+//  3rd party components:
+//---------------------------------------------
+//= require jquery.js
+//= require holder.js
+//= require elastislide/tmpl.min.js
+//= require elastislide/easing.1.3.js
+//= require elastislide/elastislide.js
+//= require elastislide/gallery.js
+//
+//---------------------------------------------
+//  Bootstrap components:
+//---------------------------------------------
+//= require bootstrap/bootstrap-transition.js
+//= require bootstrap/bootstrap-alert.js
+//= require bootstrap/bootstrap-modal.js
+//= require bootstrap/bootstrap-dropdown.js
+//= require bootstrap/bootstrap-scrollspy.js
+//= require bootstrap/bootstrap-tab.js
+//= require bootstrap/bootstrap-tooltip.js
+//= require bootstrap/bootstrap-popover.js
+//= require bootstrap/bootstrap-button.js
+//= require bootstrap/bootstrap-collapse.js
+//= require bootstrap/bootstrap-carousel.js
+//= require bootstrap/bootstrap-typeahead.js
+//= require bootstrap/bootstrap-affix.js
+//
+//---------------------------------------------
+//  Additional components:
+//---------------------------------------------
+//= require custom.js
+
